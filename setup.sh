c5018db5fc4d56a8540f47606035eeace7318283
graham44 <g.errington@gmail.com>
2015-12-24 11:27:18 -0500

    Updated setup file

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ sudo apt-get install fbi -y
 sudo apt-get install sendemail -y
 sudo apt-get install libnet-ssleay-perl -y
 # Update config.txt to bigger text.
+# Now includes Pibrella add on board
+sudo apt-get install python-pip
+sudo pip install pibrella
+git clone https://github.com/pimoroni/pibrella
