10b90865c128b69036b8971f2016f58bf594c8c3
Graham <Graham44@users.noreply.github.com>
2015-12-26 18:19:42 -0500

    Updated for Scroll Phat

--- setup.sh
+++ setup.sh
@@ -8,3 +8,6 @@ sudo apt-get install libnet-ssleay-perl -y
 sudo apt-get install python-pip
 sudo pip install pibrella
 git clone https://github.com/pimoroni/pibrella
+# Scroll PHAT now installed
+curl -sSL get.pimoroni.com/scrollphat | bash
+
